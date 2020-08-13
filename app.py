44419fa31532a0bd3c0da3027aaa879e42566a32
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2020-08-13 10:37:03 +0900

    eventlet 導入

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 from threading import Lock
 from flask import Flask, render_template, session, request, \
-    copy_current_request_context
+    copy_current_request_context, send_from_directory
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
+import os
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = None
+async_mode = "eventlet"
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+# app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
 thread = None
 thread_lock = Lock()
@@ -25,7 +26,12 @@ def background_thread():
         count += 1
         socketio.emit('my_response',
                       {'data': 'Server generated event', 'count': count},
-                      namespace='/test')
+                      )
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static/img'), 'favicon.ico', )
 
 
 @app.route('/')
@@ -33,14 +39,14 @@ def index():
     return render_template('test.html', async_mode=socketio.async_mode)
 
 
-@socketio.on('my_event', namespace='/test')
+@socketio.on('my_event')
 def test_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
          {'data': message['data'], 'count': session['receive_count']})
 
 
-@socketio.on('my_broadcast_event', namespace='/test')
+@socketio.on('my_broadcast_event')
 def test_broadcast_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
@@ -48,7 +54,7 @@ def test_broadcast_message(message):
          broadcast=True)
 
 
-@socketio.on('join', namespace='/test')
+@socketio.on('join')
 def join(message):
     join_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
@@ -57,7 +63,7 @@ def join(message):
           'count': session['receive_count']})
 
 
-@socketio.on('leave', namespace='/test')
+@socketio.on('leave')
 def leave(message):
     leave_room(message['room'])
     session['receive_count'] = session.get('receive_count', 0) + 1
@@ -66,7 +72,7 @@ def leave(message):
           'count': session['receive_count']})
 
 
-@socketio.on('close_room', namespace='/test')
+@socketio.on('close_room')
 def close(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
@@ -75,7 +81,7 @@ def close(message):
     close_room(message['room'])
 
 
-@socketio.on('my_room_event', namespace='/test')
+@socketio.on('my_room_event')
 def send_room_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
@@ -83,7 +89,7 @@ def send_room_message(message):
          room=message['room'])
 
 
-@socketio.on('disconnect_request', namespace='/test')
+@socketio.on('disconnect_request')
 def disconnect_request():
     @copy_current_request_context
     def can_disconnect():
@@ -98,12 +104,12 @@ def disconnect_request():
          callback=can_disconnect)
 
 
-@socketio.on('my_ping', namespace='/test')
+@socketio.on('my_ping')
 def ping_pong():
     emit('my_pong')
 
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect')
 def test_connect():
     global thread
     with thread_lock:
@@ -112,10 +118,10 @@ def test_connect():
     emit('my_response', {'data': 'Connected', 'count': 0})
 
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected', request.sid)
 
 
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+# if __name__ == '__main__':
+    # socketio.run(app, debug=True)
